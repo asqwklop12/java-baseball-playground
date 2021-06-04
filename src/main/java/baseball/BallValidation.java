a27cd62c7beaf6f9105151c9015bd9d0a8053b9f
asqwklop12 <asqwklop12@gmail.com>
2021-06-05 03:22:13 +0900

    feat 3자리 숫자인가?

--- src/main/java/baseball/BallValidation.java
+++ src/main/java/baseball/BallValidation.java
@@ -1,11 +1,19 @@
 package baseball;
 
+import java.util.List;
+
 public class BallValidation {
 
   private static final int MIN = 1;
   private static final int MAX = 9;
+  private static final int TOTAL_BASEBALL_CNT = 3;
+
 
   public static boolean range(int num) {
     return MIN <= num && num <= MAX;
+  }
+
+  public static boolean size(List<Integer> nums) {
+    return nums.size() == TOTAL_BASEBALL_CNT;
   }
 }
--- src/test/java/baseball/BallValidationTest.java
+++ src/test/java/baseball/BallValidationTest.java
@@ -2,6 +2,8 @@ package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
+import java.util.Collections;
 import org.junit.jupiter.api.Test;
 
 public class BallValidationTest {
@@ -12,5 +14,13 @@ public class BallValidationTest {
     assertThat(BallValidation.range(0)).isFalse();
     assertThat(BallValidation.range(5)).isTrue();
     assertThat(BallValidation.range(10)).isFalse();
+  }
+
+  @Test
+  void _3자리_숫자인가() {
+    assertThat(BallValidation.size(Arrays.asList(1, 2, 3))).isTrue();
+    assertThat(BallValidation.size(Collections.singletonList(1))).isFalse();
+    assertThat(BallValidation.size(Collections.emptyList())).isFalse();
+    assertThat(BallValidation.size(Arrays.asList(1, 2, 3, 4))).isFalse();
   }
 }
