130a505e48a1a5d248e48b2c0058f60b6881177b
asqwklop12 <asqwklop12@gmail.com>
2021-06-06 02:58:27 +0900

    refactor

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -5,6 +5,23 @@ import java.util.List;
 
 public class Game {
 
+  private static final int BASEBALL_MAX_COUNT = 3;
+  private List<Integer> computerNums;
+  private List<Integer> playerNums;
+
+  public Game() {
+  }
+
+  public Game(List<Integer> computerNums, List<Integer> playerNums) {
+    this.computerNums = computerNums;
+    this.playerNums = playerNums;
+  }
+
+  public void input(List<Integer> playerNums) {
+    this.playerNums = playerNums;
+  }
+
+
   public BallStatus compare(Ball player, Ball computer) {
     if (player.getNum() == computer.getNum() && player.getPosition() == computer.getPosition()) {
       return BallStatus.STRIKE;
@@ -15,46 +32,33 @@ public class Game {
     return BallStatus.NOTTING;
   }
 
-  public int compare(List<Integer> computer, Ball player) {
-    return iterateComputer(computer, player, 0, 0);
+
+  public int compare() {
+    return iterateComputer(0, 0);
   }
 
-  public int compare(List<Integer> computer, List<Integer> player) {
-    return iterateComputer2(computer, player, 0, 0);
-  }
-
-  private int iterateComputer2(List<Integer> computer, List<Integer> player, int comCnt, int score) {
-    if (computer.size() == comCnt) {
+  private int iterateComputer(int cnt, int score) {
+    if (cnt == BASEBALL_MAX_COUNT) {
       return score;
     }
-    score = iteratePlayer(computer, player, comCnt, 0, score);
-    return iterateComputer2(computer, player, comCnt + 1, score);
+    return iterateComputer(cnt + 1, iteratePlayer(cnt, 0, score));
   }
 
-  private int iteratePlayer(List<Integer> computer, List<Integer> player, int comCnt, int playerCnt, int score) {
-    if (player.size() == playerCnt) {
+  private int iteratePlayer(int com, int cnt, int score) {
+    if (cnt == BASEBALL_MAX_COUNT) {
       return score;
     }
-    if (compare(new Ball(comCnt + 1, computer.get(comCnt)), new Ball(playerCnt + 1, player.get(playerCnt))) == BallStatus.BALL) {
+
+    Ball computer = new Ball(com + 1, computerNums.get(com));
+    Ball player = new Ball(cnt + 1, playerNums.get(cnt));
+
+    if (compare(computer, player) == BallStatus.BALL) {
       score += 1;
     }
-    if (compare(new Ball(comCnt + 1, computer.get(comCnt)), new Ball(playerCnt + 1, player.get(playerCnt))) == BallStatus.STRIKE) {
+    if (compare(computer, player) == BallStatus.STRIKE) {
       score += 10;
     }
-    return iteratePlayer(computer, player, comCnt, playerCnt + 1, score);
-  }
-
-  private int iterateComputer(List<Integer> computer, Ball player, int cnt, int score) {
-    if (cnt == computer.size()) {
-      return score;
-    }
-    if (compare(new Ball(cnt + 1, computer.get(cnt)), player) == BallStatus.BALL) {
-      score += 1;
-    }
-    if (compare(new Ball(cnt + 1, computer.get(cnt)), player) == BallStatus.STRIKE) {
-      score += 10;
-    }
-    return iterateComputer(computer, player, cnt += 1, score);
+    return iteratePlayer(com, cnt + 1, score);
   }
 
 }
--- src/test/java/baseball/BallsTest.java
+++ src/test/java/baseball/BallsTest.java
@@ -2,54 +2,55 @@ package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import baseball.model.Ball;
 import java.util.Arrays;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.Collections;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BallsTest {
 
-  Game game;
-
-  @BeforeEach
-  void setUp() {
-    game = new Game();
-  }
-
 
   @Test
   @DisplayName("낫싱일 경우")
   void nottingTest() {
-    assertThat(game.compare(Arrays.asList(1, 2, 3), new Ball(1, 8))).isEqualTo(0);
+    Game game = new Game(Arrays.asList(1, 2, 3), Collections.singletonList(5));
+    assertThat(game.compare()).isEqualTo(0);
   }
 
   @Test
   @DisplayName("볼일 경우")
   void ballTest() {
-    assertThat(game.compare(Arrays.asList(1, 2, 3), new Ball(1, 3))).isEqualTo(1);
+    Game game = new Game(Arrays.asList(1, 2, 3), Collections.singletonList(3));
+    assertThat(game.compare()).isEqualTo(1);
   }
 
   @Test
   @DisplayName("스트라이크일 경우")
   void strikeTest() {
-    assertThat(game.compare(Arrays.asList(1, 2, 3), new Ball(1, 1))).isEqualTo(10);
+    Game game = new Game(Arrays.asList(1, 2, 3), Collections.singletonList(1));
+    assertThat(game.compare()).isEqualTo(10);
   }
 
   @Test
   @DisplayName("볼일 경우 숫자 3개 비교")
   void ballCompare3Test() {
-    assertThat(game.compare(Arrays.asList(1, 2, 3), Arrays.asList(4, 1, 5))).isEqualTo(1);
-    assertThat(game.compare(Arrays.asList(1, 2, 3), Arrays.asList(2, 1, 5))).isEqualTo(2);
-    assertThat(game.compare(Arrays.asList(1, 2, 3), Arrays.asList(2, 3, 1))).isEqualTo(3);
+    Game game = new Game(Arrays.asList(1, 2, 3), Arrays.asList(4, 1, 5));
+    assertThat(game.compare()).isEqualTo(1);
+    game.input(Arrays.asList(2, 1, 5));
+    assertThat(game.compare()).isEqualTo(2);
+    game.input(Arrays.asList(2, 3, 1));
+    assertThat(game.compare()).isEqualTo(3);
   }
 
   @Test
   @DisplayName("스트라이크일 경우 숫자 3개 비교")
   void strikeCompare3Test() {
-    assertThat(game.compare(Arrays.asList(1, 2, 3), Arrays.asList(1, 4, 5))).isEqualTo(10);
-    assertThat(game.compare(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 5))).isEqualTo(20);
-    assertThat(game.compare(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3))).isEqualTo(30);
+    Game game = new Game(Arrays.asList(1, 2, 3), Arrays.asList(1, 4, 5));
+    assertThat(game.compare()).isEqualTo(10);
+    game.input(Arrays.asList(1, 2, 5));
+    assertThat(game.compare()).isEqualTo(20);
+    game.input(Arrays.asList(1, 2, 3));
+    assertThat(game.compare()).isEqualTo(30);
   }
 
 
