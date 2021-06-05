9fd099e54dec443def8d0fa6b4ca5fee08873ff9
asqwklop12 <asqwklop12@gmail.com>
2021-06-06 03:05:42 +0900

    test 필요없는 import 제거

--- src/test/java/baseball/BallsTest.java
+++ src/test/java/baseball/BallsTest.java
@@ -3,7 +3,6 @@ package baseball;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
-import java.util.Collections;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
