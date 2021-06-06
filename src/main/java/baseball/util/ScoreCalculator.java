cc844dcdd8fe86a05b92a57e7dc755b1f996c2a0
asqwklop12 <asqwklop12@gmail.com>
2021-06-06 18:18:25 +0900

    test 필요없는 로직 제거

--- src/main/java/baseball/util/ScoreCalculator.java
+++ src/main/java/baseball/util/ScoreCalculator.java
@@ -1,24 +1,24 @@
 package baseball.util;
 
-import java.util.List;
-
 public class ScoreCalculator {
 
-  public static String convert(int score) {
+
+  public static String action(int score) {
+    String[] result = split(convert(score));
+    if (result.length == 1) {
+      return result[0] + "볼";
+    }
+    if (result[1].equals("0")) {
+      return result[0] + "스트라이크";
+    }
+    return result[1] + "볼 " + result[0] + "스트라이크";
+  }
+
+  private static String convert(int score) {
     return String.valueOf(score);
   }
 
-  public static String[] split(String score) {
+  private static String[] split(String score) {
     return score.split("");
-  }
-
-  public static String action(List<String> score) {
-    if (score.size() == 1) {
-      return score.get(0) + "볼";
-    }
-    if (score.get(1).equals("0")) {
-      return score.get(0) + "스트라이크";
-    }
-    return score.get(1) + "볼 " + score.get(0) + "스트라이크";
   }
 }
